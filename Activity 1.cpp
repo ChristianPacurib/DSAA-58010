0bde2162c1003710f0ea2fa48d091a9d34076126
ChristianPacurib <123444266+ChristianPacurib@users.noreply.github.com>
2023-08-18 08:42:21 +0800

    Add files via upload

---
+++ Activity 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len, dia, area;
+    cout<<"Enter Length of Rectangle: ";
+    cin>>len;
+    cout<<"Enter Diameter of Rectangle: ";
+    cin>>dia;
+    area = len*dia;
+    cout<<"\nArea = "<<area;
+    cout<<endl;
+    return 0;
+}
